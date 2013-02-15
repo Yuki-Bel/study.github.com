906b054e8a136a05288a55bede662c3c8023e7ec
yuki <yuki@yuki-MacBook-Air.local>
2013-02-15 23:56:41 +0900

    sudoku into git

---
+++ code_study/Sudoku/src/org/example/sudoku/Game.java
@@ -0,0 +1,35 @@
+package org.example.sudoku;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class Game extends Activity {
+
+	private static final String TAG = "Sudoku";
+
+	public static final String KEY_DIFFICULTY = "difficulty";
+	public static final int DIFFICULTY_EASY = 0;
+	public static final int DIFFICULTY_MEDIUM = 1;
+	public static final int DIFFICULTY_ = 2;
+
+	private int puzzle[] = new int[9 * 9];
+
+	// private PuzzleView puzzleView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate");
+
+		int diff = getIntent().getIntExtra(KEY_DIFFICULTY, DIFFICULTY_EASY);
+		// puzzle = getPuzzle(diff);
+		// calculateUsedTiles();
+
+		// puzzleView = newpuzzleView;
+		// setContentView(puzzleView);
+		// puzzleView.requestFocus}();
+	}
+	// ...
+}
---
+++ code_study/Sudoku/src/org/example/sudoku/MainActivity.java
@@ -0,0 +1,92 @@
+package org.example.sudoku;
+
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+//インターフェースの継承はimplements ただし継承した場合はoverrideか再定義が必要
+public class MainActivity extends Activity implements OnClickListener {
+
+	private static final String TAG = "Sudoku";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// すべてのボタンについてクリックリスナーをセットアップする
+		// this = 自身のクラス（インスタンス）そのもの
+		View continueButton = this.findViewById(R.id.continue_button);
+		continueButton.setOnClickListener((OnClickListener) this);
+		View newButton = this.findViewById(R.id.new_button);
+		newButton.setOnClickListener((OnClickListener) this);
+		View aboutButton = this.findViewById(R.id.about_button);
+		aboutButton.setOnClickListener((OnClickListener) this);
+		View exitButton = this.findViewById(R.id.exit_button);
+		exitButton.setOnClickListener((OnClickListener) this);
+	}
+
+	// onClick定義　タップ時の反応＝intent（Activity）呼び出し
+	// @Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.new_button:
+			openNewGameDialog();
+			break;
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		case R.id.exit_button:
+			finish();
+			break;
+		}
+	}
+
+	// onCreateOptionsMenu定義　メニューボタンのxml有効化
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	// メニュー項目選択後のintent呼び出し
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, Settings.class));
+			return true;
+		}
+		return false;
+
+	}
+
+	private void openNewGameDialog() {
+		new AlertDialog.Builder(this)
+				.setTitle(R.string.new_game_title)
+				.setItems(R.array.difficulty,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog, int i) {
+								// TODO Auto-generated method stub
+								startGame(i);
+
+							}
+						}).show();
+	}
+
+	/* 指定された難易度レベルで新しいゲームを開始する　 */
+	private void startGame(int i) {
+		Log.d(TAG, "clicke on " + i);
+		// start geme here
+	}
+}
